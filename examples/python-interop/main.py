c1f512a7fe08ef8989e489456db3e4444036e02d
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2024-08-27 14:42:13 +0200

    add some imports

--- examples/python-interop/main.py
+++ examples/python-interop/main.py
@@ -1,3 +1,36 @@
+import socket
+import select
+import logging
+import time
+from threading import Thread
+from typing import Tuple, Optional
+from multiprocessing import Queue
+from queue import Empty
+
+from cfdppy import PacketDestination, PutRequest, get_packet_destination, CfdpState
+from cfdppy.mib import (
+    CheckTimerProvider,
+    DefaultFaultHandlerBase,
+    EntityType,
+    IndicationCfg,
+    RemoteEntityCfg,
+)
+from spacepackets.cfdp.pdu import AbstractFileDirectiveBase, PduFactory, PduHolder
+from spacepackets.util import ByteFieldU16, UnsignedByteField
+
+_LOGGER = logging.getLogger(__name__)
+
+
+LOCAL_ENTITY_ID = ByteFieldU16(1)
+REMOTE_ENTITY_ID = ByteFieldU16(2)
+# Enable all indications for both local and remote entity.
+INDICATION_CFG = IndicationCfg()
+
+FILE_CONTENT = "Hello World!\n"
+FILE_SEGMENT_SIZE = 256
+MAX_PACKET_LEN = 512
+
+
 def main():
     pass
 
